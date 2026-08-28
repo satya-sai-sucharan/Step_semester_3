c24650f26f63cf80a599d109dd7207aac6b27cf8
satya-sai-sucharan <satyasai.p2006@gmail.com>
2026-08-28 19:04:35 +0530

    add positive Negative Zero Classifier

---
+++ control_flow/assignment_problems/NumberClassifier.java
@@ -0,0 +1,18 @@
+package control_flow.assignment_problems;
+
+public class NumberClassifier {
+
+    static void classifyNumber(int number) {
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+
+    public static void main(String[] args) {
+        classifyNumber(-4);
+    }
+}
